9a8e868c74e289d329c5a13a5bd23ec5b66beb32
ceramicatumminia <tumminiaceramiche@gmail.com>
2026-06-10 10:04:35 +0200

    header: logo a sinistra, brand centrato separato

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -35,13 +35,14 @@ export default function Header() {
   return (
     <header className={`${styles.header} ${scrolled ? styles.scrolled : ''}`}>
       <div className={styles.headerTop}>
-        <Link href="/" className={styles.logoWrap}>
+        <Link href="/" className={styles.logoOnly}>
           <img src="/logo.png" alt="Ippocampo" className={styles.logoImg} />
-          <div className={styles.brand}>
-            <span className={styles.brandMain}>Cer&apos;Amica</span>
-            <span className={styles.brandSub}>di Bruna Tumminia</span>
-          </div>
         </Link>
+        <Link href="/" className={styles.brand}>
+          <span className={styles.brandMain}>Cer&apos;Amica</span>
+          <span className={styles.brandSub}>di Bruna Tumminia</span>
+        </Link>
+        <div />
       </div>
 
       <div className={styles.headerUtils}>

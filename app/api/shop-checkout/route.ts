0951b563c1ad0ca52d33b14c32fdc2b00e38bc36
ceramicatumminia <tumminiaceramiche@gmail.com>
2026-06-10 09:41:55 +0200

    fix versione API Stripe

--- app/api/shop-checkout/route.ts
+++ app/api/shop-checkout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2025-04-30.basil' })
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-06-20' })
 
 export async function POST(req: NextRequest) {
   try {

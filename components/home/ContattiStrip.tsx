e1e5354b48e0723fb1f58245c1667cac3a6e3f08
ceramicatumminia <tumminiaceramiche@gmail.com>
2026-06-15 13:58:23 +0200

    font Cormorant Garamond nel footer

--- components/home/ContattiStrip.tsx
+++ components/home/ContattiStrip.tsx
@@ -1,5 +1,14 @@
 import styles from '@/styles/site.module.css'
 
+const textStyle = {
+  fontFamily: 'Cormorant Garamond, serif',
+  fontSize: '15px',
+  lineHeight: 1.8,
+  color: '#e0d4c8',
+  textDecoration: 'none',
+  display: 'block',
+} as const
+
 export default function ContattiStrip() {
   return (
     <section className={styles.footer}>
@@ -9,13 +18,13 @@ export default function ContattiStrip() {
       </div>
       <div>
         <div className={styles.labelD}>Laboratorio</div>
-        <p className={styles.bodySmallD}>Via Decimo 8<br/>09026 San Sperate (SU)<br/>340 0045472</p>
-        <a href="mailto:ceramicatumminia@gmail.com" className={styles.linkD} style={{marginTop:'12px'}}>ceramicatumminia@gmail.com</a>
+        <p style={textStyle}>Via Decimo 8<br/>09026 San Sperate (SU)<br/>340 0045472</p>
+        <a href="mailto:ceramicatumminia@gmail.com" style={{...textStyle,marginTop:'12px'}}>ceramicatumminia@gmail.com</a>
       </div>
       <div>
         <div className={styles.labelD}>Social</div>
-        <a href="https://www.instagram.com/ceramicatumminia/" target="_blank" rel="noreferrer" className={styles.linkD} style={{fontStyle:'italic',marginBottom:'6px'}}>Instagram @ceramicatumminia</a>
-        <a href="https://www.facebook.com/p/CerAmica-Tumminia-100063630775468/" target="_blank" rel="noreferrer" className={styles.linkD} style={{fontStyle:'italic'}}>Facebook Cer'Amica Tumminia</a>
+        <a href="https://www.instagram.com/ceramicatumminia/" target="_blank" rel="noreferrer" style={{...textStyle,marginBottom:'6px'}}>Instagram @ceramicatumminia</a>
+        <a href="https://www.facebook.com/p/CerAmica-Tumminia-100063630775468/" target="_blank" rel="noreferrer" style={textStyle}>Facebook Cer&apos;Amica Tumminia</a>
       </div>
     </section>
   )
